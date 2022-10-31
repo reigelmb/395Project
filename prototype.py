e31168f0b70d7a0e1a8d3773d335de9c891d93d7
aridae1 <113487100+aridae1@users.noreply.github.com>
2022-10-31 13:00:14 -0400

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -15,16 +15,34 @@ class Window(Frame):
     #Declare
 
     def __init__(self, file, window=None):
+        Frame.__init__(self, window)
         self.window = window
+        self.window.attributes('-fullscreen', True)
         self.file = file
+        self.frame = Frame(self.window, bg="black")
+        self.frame.grid(row=0, column=0, padx=10, pady=10)
+        self.title = Label(self.frame, text="Typical Monday", justify=CENTER, font=("Roman", 20), fg="orange", bg="black", width=100, wraplength=700)
+        self.title.grid(row=1, column=1, columnspan=6)
+        self.DIE = Button(self.frame, text="Die", width=10, command=self.clickDieButton)
+        self.DIE.grid(row=2, column=3)
+        self.START = Button(self.frame, text="Start", width=10, command=self.clickStartButton)
+        self.START.grid(row=2, column=2)
+
+    def clickStartButton(self):
+        '''self.dialog.destroy()
+        self.DIE.destroy()
+        self.START.destroy()'''
+        self.frame.destroy()
+        self.start()
+
+    def start(self):
         self.numButtons = 2
         self.window["bg"] = "black"
         self.frame = Frame(self.window, bg="black")
-        self.frame.grid(row=0, column=0, padx=10, pady=10)
+        self.frame.grid(row=1, column=1, padx=10, pady=10)
         self.dialog = Label(self.frame, text=self.file["Scenario"]["Scenario1"]["Dialogue"], justify=CENTER, font="Roman", fg="orange", bg="black", width=100, wraplength=700)
         self.dialog.grid(row=0, column=0, columnspan=3, rowspan=2, pady=40, sticky="n")
         self.dialog.grid_rowconfigure(0, weight=1)
-        #self.dialog.pack()
         if self.file["Scenario"]["Scenario1"]["Dec1"] != "":
             self.option1 = Button(self.frame, text=self.file["Scenario"]["Scenario1"]["Dec1"], command=lambda: self.buttons(self.file['Scenario']['Scenario1']['pointer1']), justify=CENTER)
             self.option1.grid(row=3, column=0, sticky="s", rowspan=2, pady=100)
@@ -36,7 +54,12 @@ class Window(Frame):
         if self.file["Scenario"]["Scenario1"]["Dec3"] != "":
             self.option3 = Button(self.window, text=self.file["Scenario"]["Scenario1"]["Dec3"], command=lambda: self.buttons(self.file['Scenario']['Scenario1']['pointer3']), justify=CENTER)
             self.option3.grid(row=3, column=2, sticky="s", rowspan=2, pady=100)
-            #self.option3.pack()
+        self.menu = Menu(self.window, tearoff=0, bg="white", fg="orange")
+        self.menu.add_command(label="Die", command=self.clickDieButton)
+        self.menu.add_command(label="Inventory")
+        self.menu.add_command(label="Help")
+        self.window.config(menu=self.menu)
+
 
     def buttons(self, currentScenario):
         self.dialog.destroy()
